362b071e5854b4cd63c774891a221fed0ec7ac0a
Maxim Chen <chenr.maxim@gmail.com>
2024-04-04 04:12:32 -0700

    feat: add create_user route, update user model

--- Backend/api/models/UserModel.py
+++ Backend/api/models/UserModel.py
@@ -1,13 +1,13 @@
-from pydantic import BaseModel, Field, ValidationError, field_validator, ValidationInfo
+from pydantic import BaseModel, Field, ValidationError, field_validator, ValidationInfo, EmailStr
 from uuid import uuid4
-from typing import Optional
+from typing import Optional, Annotated
 from datetime import datetime
 
 class User(BaseModel):
-    id : str = Field(default_factory=lambda: uuid4().hex)
+    id : Annotated[str, Field(default_factory=lambda: uuid4().hex)]
     username : str
-    email : str = Field(pattern=r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
-    created_at: datetime = datetime.now()
+    email : EmailStr
+    created_at: datetime = Field(default_factory=datetime.now)
 
     @field_validator('username')
     @classmethod
--- Backend/api/routes/user.py
+++ Backend/api/routes/user.py
@@ -6,7 +6,12 @@ from models import UserModel
 router = APIRouter()
 
 @router.get("/listall", response_description="List of all Users", response_model=List[UserModel.User])
-async def list_users(request: Request):
-    users = [user async for user in request.app.user_items_container.read_all_items()]
+async def list_users(req: Request):
+    users = [user async for user in req.app.user_items_container.read_all_items()]
     return users
 
+@router.post("/user", response_model=UserModel.User)
+async def create_user(req: Request, user: UserModel.User):
+    user = jsonable_encoder(user)
+    new_user = await req.app.user_items_container.create_item(user)
+    return new_user
