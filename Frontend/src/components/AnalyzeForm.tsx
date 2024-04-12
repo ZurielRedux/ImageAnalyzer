5d5fe4c972966e15499f1aa278780d525d5b26b8
Maxim Chen <chenr.maxim@gmail.com>
2024-04-12 02:17:58 -0700

    feat: show user chosen image, add button and form styling

--- Frontend/src/components/AnalyzeForm.tsx
+++ Frontend/src/components/AnalyzeForm.tsx
@@ -2,8 +2,12 @@ import { ChangeEvent, FormEvent, useState } from "react";
 import { IForm, initialAnalyzeFormState } from "@/ts/interfaces/form";
 import * as API from "@/util/api";
 
+import styles from "@/styles/analyze.module.scss";
+
 const AnalyzeForm = () => {
   const [form, setForm] = useState<IForm>(initialAnalyzeFormState);
+  const [fileUploaded, setFileUploaded] = useState<boolean>(false);
+  const [image, setImage] = useState<string | null>(null);
 
   const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();
@@ -52,33 +56,55 @@ const AnalyzeForm = () => {
       return;
     }
     const selectedFile = target.files[0];
+    setImage(URL.createObjectURL(target.files[0]));
 
     setForm((prevForm) => ({
       ...prevForm,
       file: selectedFile,
     }));
+    setFileUploaded(true);
   };
 
+  const buttonsRow = (
+    <div className={styles["btns-row"]}>
+      <button className={styles["upload-btn"]} type="submit">
+        upload image
+      </button>
+      <button onClick={onClick} type="submit">
+        analyze image
+      </button>
+    </div>
+  );
+
   return (
-    <div>
+    <div className={styles["analyze-form-container"]}>
       <form
         onSubmit={handleSubmit}
         id="uploadForm"
         encType="multipart/form-data"
+        className={styles["form-container"]}
       >
-        <label htmlFor="fileInput">Choose a image</label>
-        <input
-          type="file"
-          id="fileInput"
-          name="image-file"
-          accept="image/*"
-          onChange={handleFileChange}
-        ></input>
-        <button type="submit">upload image</button>
+        <div className={styles["file-input-container"]}>
+          <label
+            className={styles["file-input-upload-btn"]}
+            htmlFor="fileInput"
+          >
+            Choose an image
+          </label>
+          <input
+            type="file"
+            id="fileInput"
+            name="image-file"
+            accept="image/*"
+            onChange={handleFileChange}
+          />
+        </div>
+        <div className={styles["image-container"]}>
+          <span>{form.file?.name}</span>
+          {image && <img src={image} alt="user uploaded image" />}
+        </div>
+        {fileUploaded ? buttonsRow : <></>}
       </form>
-      <button onClick={onClick} type="submit">
-        analyze image
-      </button>
     </div>
   );
 };
--- Frontend/src/pages/Analyze.tsx
+++ Frontend/src/pages/Analyze.tsx
@@ -1,9 +1,17 @@
 import AnalyzeForm from "@/components/AnalyzeForm";
 
+import styles from "@/styles/analyze.module.scss";
+
 const Analyze = () => {
   return (
-    <div>
-      Welcome to Analyze page
+    <div className={styles["analyze-page-container"]}>
+      <div className={styles["analyze-header"]}>
+        <h2>Analyze Image</h2>
+        <p>
+          Upload your image to view various visual stats and features like
+          categories, colors, objects, and information tags
+        </p>
+      </div>
       <AnalyzeForm />
     </div>
   );
