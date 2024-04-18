07c0483912faa8a80b0d695e6643f1b96d38b92c
Maxim Chen <chenr.maxim@gmail.com>
2024-04-17 21:11:26 -0700

    style: navbar style fixes

--- Frontend/src/components/NavLink.tsx
+++ Frontend/src/components/NavLink.tsx
@@ -3,17 +3,12 @@ import { NavLink as NavlinkBase, NavLinkProps } from "react-router-dom";
 
 import styles from "@/styles/navlink.module.scss";
 
-// interface ICustomNavLinkProps extends NavLinkProps {
-//   className: string;
-// }
-
 export const NavLink = React.forwardRef<HTMLAnchorElement, NavLinkProps>(
   (props, ref) => {
     return (
       <NavlinkBase
         ref={ref}
         {...props}
-        // className={styles["navlink-base-container"]}
         className={({ isActive }) =>
           `${styles["navlink-base-container"]} ${
             isActive ? `${styles["active"]}` : ""
